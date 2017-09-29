9141c93939e28200992a4704542456cd88f7d69e
Felix Schulenburg <felix.schulenburg@gmail.com>
2017-09-29 16:08:19 +0200

    Switched to a last.fm powered now playing view

---
+++ js/index.js
@@ -0,0 +1,97 @@
+var ListenWithMe = (function() {
+        
+  // Add your own config here
+  var LASTFM_API_KEY = "dbad2bc6bc80b741aa8c272d33c9d863";
+  var LASTFM_USERNAME = "itchy1337";
+  var USER_NAME = "Felix"
+
+  // Elements
+  var $trackTitle = document.getElementsByClassName('track-info--title')[0]
+  var $trackArtist = document.getElementsByClassName('track-info--artist')[0]
+  var $trackAlbum = document.getElementsByClassName('track-info--album')[0]
+  var $trackImage = document.getElementsByClassName('track-artwork--image')[0]
+
+  var currentSong = {};
+  var REFRESH_INTERVAL = 10000;
+  var refreshTimer = null;
+
+  return {
+    init: init
+  }
+
+  function init() {
+    getCurrentTrack().then(function(data) {
+      console.log(data)
+      try {
+        currentSong.nowPlaying = data['@attr'].nowplaying;        
+      } catch(err) {
+        console.log(err);
+      }
+
+      currentSong.track = data.name;
+      currentSong.artist = data.artist['#text'];
+      currentSong.album = data.album['#text'];
+      currentSong.image = data.image[3]['#text'];
+      updateTitle();
+      updateInfo();
+
+    }).then(function(data) {
+      console.log(data)
+    })
+
+    if(!refreshTimer) {
+      refreshTimer = setInterval(refresh, REFRESH_INTERVAL)
+    }
+  }
+
+  function refresh() {
+    console.log("ping");
+    getCurrentTrack().then(function(data) {
+      if(currentSong.track !== data.name && currentSong.track !== data.artist['#text']) {
+        // kinda lazy
+        init();
+      }
+    })
+  }
+
+  // Make a call to Last FM to get your latest played tracks;
+  function getCurrentTrack() { 
+    var url = "//ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + LASTFM_USERNAME + "&api_key=" + LASTFM_API_KEY + "&format=json";
+
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+      req.onload = function() {
+        if (req.status == 200) {
+          var data = JSON.parse(req.response);
+          try {
+            resolve(data.recenttracks.track[0]);
+          } catch(err) {
+            reject(err);
+          }
+        }
+        else {
+          reject(Error(req.statusText));
+        }
+      };
+      req.onerror = function() {
+        reject(Error("Network Error"));
+      };
+      req.send();
+    })
+  }
+
+  function updateTitle() {
+    document.title = currentSong.track + " by " + currentSong.artist;
+  }
+
+  function updateInfo() {
+    $trackTitle.innerHTML = currentSong.track;
+    $trackArtist.innerHTML = currentSong.artist;
+    $trackAlbum.innerHTML = currentSong.album;
+    $trackImage.src = currentSong.image.replace('/300x300', '');
+  }
+
+})();
+
+ListenWithMe.init();
